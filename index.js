25d2dd2016eea0a6d8863aaddd507686de405dbb
Idris Bowman <bowmanidris95@gmail.com>
2020-09-28 21:08:00 -0400

    add resolvers and dataSources

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { ApolloServer, gql } = require('apollo-server');
+const SessionAPI = require('./datasources/sessions');
 
 // Define Schema
 const typeDefs = gql`
@@ -11,13 +12,25 @@ type Session {
     description: String,
     startAt: String,
     endsAt: String,
+    room: String,
     day: String,
     format: String,
-    track: String,
+    track: String @deprecated(reason:"Too many seesion do not fit into a single track, we will be mirating to a tags based system the future..."),
     level:String
 }`
 
-const server = new ApolloServer({ typeDefs });
+const resolvers = {
+    Query: {
+        sessions: (parent, args, { dataSources }, info) => {
+            return dataSources.SessionAPI.getSessions();
+        }
+    }
+};
+
+const dataSources = () => ({
+    sessionAPI: new SessionAPI()
+});
+const server = new ApolloServer({ typeDefs, resolvers, dataSources });
 
 server
     .listen({ port: process.env.PORT || 4000})
